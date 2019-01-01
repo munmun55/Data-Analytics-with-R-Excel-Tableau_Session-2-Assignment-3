68d213b8d4145be0d5db30c8842939c72cc98f58
munmun55 <44106801+munmun55@users.noreply.github.com>
2019-01-02 02:39:10 +0530

    Update Assignment 2_3_question4.R

--- Assignment 2_3_question4.R
+++ Assignment 2_3_question4.R
@@ -1,34 +1,7 @@
-#1. How to Import SAS XPORT files into R with the foreign package?
-install.packages("foreign")
-library(foreign)
-read.xport("E:/munmun_acadgild/acadgild data analytics/b3/ASSIGNMENT/B3 ASSIGNMENT/sample_sas_xport_file.xpt")
-
-
-#2. How to Import SAS Files into R with the Haven package?
-
-library(haven)
-
-data_file <-"E:/munmun_acadgild/acadgild data analytics/b3/ASSIGNMENT/B3 ASSIGNMENT/airline.sas7bdat"
-read_sas(data_file, catalog_file = NULL, encoding = NULL,
-         
-         cols_only = NULL)
-
-
-
-#3. How to read Weka Attribute-Relation File Format (ARFF) files in R?
-
-#setwd()
-
-library(foreign)
-data_file <-"E:/munmun_acadgild/acadgild data analytics/b3/ASSIGNMENT/B3 ASSIGNMENT/sample_weka.arff"
-read.arff(data_file)
-
-
-
-#4. How to read a heavy csv/tsv file using readr package?
+#SESSION 2: Introduction to working with R, Assignment 3:Submitted by Munmun Ghosal
+#4. a. Read a heavy csv file using readr package
 
 library(readr)
-data_file <-"E:/munmun_acadgild/acadgild data analytics/b3/ASSIGNMENT/B3 ASSIGNMENT/sample_tsv_file.tsv"
 file <-"E:/munmun_acadgild/acadgild data analytics/b3/ASSIGNMENT/B3 ASSIGNMENT/cs2m.csv"
 read_csv(file, col_names = TRUE, col_types = NULL,
          
@@ -38,6 +11,7 @@ read_csv(file, col_names = TRUE, col_types = NULL,
          
           progress = show_progress())
 
+#4.b. Read a heavy tsv file using readr package
 library(readr)
 file <-"E:/munmun_acadgild/acadgild data analytics/b3/ASSIGNMENT/B3 ASSIGNMENT/sample_tsv_file.tsv"
 read_tsv(file, col_names = TRUE, col_types = NULL,
